17bfd7a79798ed4a0331de9456731652c71d1925
maulida34 <maulidarizky882@gmail.com>
2024-12-11 19:50:00 +0800

    modified:   bMenu/menu.cpp

--- bMenu/menu.cpp
+++ bMenu/menu.cpp
@@ -113,3 +113,19 @@ void sortingAsc()
     cout << "Data berhasil diurutkan secara ascending.\n";
     getch();
 }
+
+void sortingDsc()
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arrdata[j] < arrdata[j + 1])
+            {
+                tukar(&arrdata[j], &arrdata[j + 1]);
+            }
+        }
+    }
+    cout << "Data berhasil diurutkan secara descending.\n";
+    getch();
+}
