5ac36608082ee2a8892b47e8718eadc4fc656db3
maulida34 <maulidarizky882@gmail.com>
2024-12-25 15:28:43 +0800

    Tampilkan data

--- bMenu/menu.cpp
+++ bMenu/menu.cpp
@@ -2,9 +2,6 @@
 #include <string>
 #include <conio.h>
 using namespace std;
-// menu
-int data[100];
-int n;
 
 struct mahasiswa
 {
@@ -16,77 +13,28 @@ struct mahasiswa
 
 mahasiswa sikc[30];
 
-int pos = -1; // index
+int pos = -1;
 
 void dMenu()
 {
     system("cls");
-    cout << "Aplikasi Sorting Bubble" << "\n";
+    cout << "Aplikasi KelasQ" << "\n";
     cout << "1. Masukkan data" << "\n";
     cout << "2. Tampilkan data" << "\n";
-    cout << "3. Sorting asc" << "\n";
-    cout << "4. Sorting dsc" << "\n";
+    cout << "3. Perbaikan data" << "\n";
+    cout << "4. Menghapus data" << "\n";
     cout << "5. Exit" << "\n";
     cout << "Masukan angka :";
 }
 
-void tukar(int *a, int *b)
+void tampilkanData(int p)
 {
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
-
-void tampilkanData(int data[], int n)
-{
-    if (n > 0)
-    {
-        cout << "Data saat ini:\n";
-        for (int i = 0; i < n; i++)
-        {
-            cout << "Data ke-" << (i + 1) << ": " << data[i] << "\n";
-        }
-    }
-    else
-    {
-        cout << "Data kosong. Masukkan data terlebih dahulu!";
-    }
-}
-// sorting asc
-void sortingAscending(int data[], int n)
-{
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (data[j] > data[j + 1])
-            {
-                tukar(&data[j], &data[j + 1]);
-            }
-        }
-    }
-    cout << "Data berhasil diurutkan secara ascending!";
-}
-
-void sortingDescending(int data[], int n)
-{
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (data[j] < data[j + 1])
-            {
-                tukar(&data[j], &data[j + 1]);
-            }
-        }
-    }
-    cout << "Data berhasil diurutkan secara descending!";
+    cout << sikc[p].nim << " , " << sikc[p].nama << " , " << sikc[p].alamat << " , " << sikc[p].ipk << endl;
 }
 
 int main()
 {
-    int data[100];
-    char pl;
+    int pos = -1;
 
     do
     {
@@ -109,41 +57,20 @@ int main()
             cout << "masukan ipk: ";
             cin >> sikc[pos].ipk;
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            break;
         }
         case '2':
         {
-            system("cls");
-            tampilkanData(data, n);
-            getch();
+            cout << sikc[p].nim << " , " << sikc[p].nama << " , " << sikc[p].alamat << " , " << sikc[p].ipk << endl;
             break;
         }
         case '3':
         {
-            system("cls");
-            if (n > 0)
-            {
-                sortingAscending(data, n);
-            }
-            else
-            {
-                cout << "Data kosong. Masukkan data terlebih dahulu!";
-            }
-            getch();
-            break;
+            
         }
         case '4':
         {
-            system("cls");
-            if (n > 0)
-            {
-                sortingDescending(data, n);
-            }
-            else
-            {
-                cout << "Data kosong. Masukkan data terlebih dahulu!";
-            }
-            getch();
-            break;
+            
         }
         case '5':
             break;
